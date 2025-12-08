6d8fa887c420865b30737748691f71c9a34b126f
Bagas <108858099+VampXDH@users.noreply.github.com>
2025-12-09 05:49:44 +0700

    Create writer.go

---
+++ internal/scanner/writer.go
@@ -0,0 +1,54 @@
+package scanner
+
+import (
+	"os"
+	"sync"
+)
+
+type OutputWriter struct {
+	file     *os.File
+	mu       sync.Mutex
+	seen     map[string]bool
+	filename string
+	count    int
+}
+
+func NewOutputWriter(filename string) *OutputWriter {
+	// Create directory if needed
+	_ = os.MkdirAll("results", 0755)
+
+	file, err := os.Create(filename)
+	if err != nil {
+		panic(err)
+	}
+
+	return &OutputWriter{
+		file:     file,
+		seen:     make(map[string]bool),
+		filename: filename,
+		count:    0,
+	}
+}
+
+func (w *OutputWriter) Write(domain string) {
+	w.mu.Lock()
+	defer w.mu.Unlock()
+
+	if w.seen[domain] {
+		return
+	}
+
+	w.seen[domain] = true
+	w.count++
+	w.file.WriteString(domain + "\n")
+}
+
+func (w *OutputWriter) Close() {
+	w.file.Close()
+}
+
+func (w *OutputWriter) Count() int {
+	w.mu.Lock()
+	defer w.mu.Unlock()
+	return w.count
+}
